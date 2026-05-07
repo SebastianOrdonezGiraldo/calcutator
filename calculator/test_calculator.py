04534e7c3949d5d25e5ef2d8153a0275606a3f5e
Sebastian Ordoñez Giraldo <140672870+SebastianOrdonezGiraldo@users.noreply.github.com>
2026-05-07 18:37:25 -0500

    Testt
    test

---
+++ calculator/test_calculator.py
@@ -0,0 +1,25 @@
+import unittest
+from calculator import Calculator
+
+class TestCalculator(unittest.TestCase):
+    
+    # ESTO ES LO QUE FALTA O ESTÁ MAL ESCRITO:
+    def setUp(self):
+        self.calc = Calculator()
+
+    def test_suma(self):
+        # Ahora self.calc ya existirá
+        self.assertEqual(self.calc.sumar(5, 5), 10)
+
+    def test_resta(self):
+        self.assertEqual(self.calc.restar(10, 5), 5)
+
+    def test_division(self):
+        self.assertEqual(self.calc.dividir(10, 2), 5)
+
+    def test_division_por_cero(self):
+        with self.assertRaises(ValueError):
+            self.calc.dividir(10, 0)
+
+if __name__ == '__main__':
+    unittest.main()
